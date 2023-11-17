f562f3816b3f31d31b08e22fbc78fd98096a158b
zheng_b <bzheng99.real@gmail.com>
2023-11-17 17:28:33 -0600

    Started trying out the Calendar Library, still need to experiment more

--- app/src/main/java/com/example/taskmanager/CalendarActivity/CalendarActivity.kt
+++ app/src/main/java/com/example/taskmanager/CalendarActivity/CalendarActivity.kt
@@ -2,8 +2,16 @@ package com.example.taskmanager.CalendarActivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+// import androidx.compose.runtime.Composable
+/*import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource*/
 import android.widget.CalendarView
 import com.example.taskmanager.R
+import java.time.DayOfWeek
+import java.time.YearMonth
+
+// private val pageBackGroundColor: Color @Composable get() =
 
 class CalendarActivity : AppCompatActivity() {
 
--- app/src/main/java/com/example/taskmanager/CalendarActivity/CalendarFragment.kt
+++ app/src/main/java/com/example/taskmanager/CalendarActivity/CalendarFragment.kt
@@ -6,7 +6,43 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.CalendarView
 import androidx.fragment.app.Fragment
+
+import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.CalendarMonth
+import com.kizitonwose.calendar.core.DayPosition
+import com.kizitonwose.calendar.core.daysOfWeek
+import com.kizitonwose.calendar.core.nextMonth
+import com.kizitonwose.calendar.core.previousMonth
+import com.kizitonwose.calendar.view.MonthDayBinder
+import com.kizitonwose.calendar.view.MonthHeaderFooterBinder
+import com.kizitonwose.calendar.view.ViewContainer
+import com.example.taskmanager.databinding.CalendarHeaderBinding
+import com.example.taskmanager.databinding.CalendarFragmentBinding
+
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.YearMonth
 import com.example.taskmanager.R
+
+class CalendarFragment : Fragment() {
+    private var selectedDate: LocalDate? = null
+    private lateinit var binding: CalendarFragmentBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = CalendarFragmentBinding.bind(view)
+
+        val daysOfWeek = daysOfWeek()
+        val currentMonth = YearMonth.now()
+        val startMonth = currentMonth.minusMonths(200)
+        val endMonth = currentMonth.plusMonths(200)
+        binding.exFiveCalendar.setup(startMonth, endMonth, daysOfWeek.first())
+        binding.exFiveCalendar.scrollToMonth(currentMonth)
+
+    }
+
+}
+/*
 class CalendarFragment : Fragment() {
     private lateinit var mCalendar: CalendarView
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,4 +60,4 @@ class CalendarFragment : Fragment() {
         return root
     }
 
-}
+}*/
