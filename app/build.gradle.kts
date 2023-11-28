3062a7d96adecea017fee9c3a2d53706cc5f8ad6
Bethany Terry <142756598+bcterry@users.noreply.github.com>
2023-11-28 16:48:50 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.room:room-common:2.6.0")
+    implementation("androidx.room:room-common:2.5.0-rc01")
+    implementation("androidx.room:room-common:2.4.3")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -55,4 +58,17 @@ dependencies {
 
     // Calendar API for Custom Calendars
     implementation("com.kizitonwose.calendar:view:2.0.4")
+
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+
+    implementation("androidx.room:room-ktx:2.4.3")
+    kapt("androidx.room:room-compiler:2.4.3")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation("androidx.activity:activity-ktx:1.6.0")
+    implementation("androidx.fragment:fragment-ktx:1.5.3")
+    annotationProcessor("android.arch.persistence.room:compiler:1.1.1")
+    implementation("androidx.room:room-runtime:2.4.3")
 }
