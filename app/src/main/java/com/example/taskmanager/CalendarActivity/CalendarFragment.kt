a8104fa1402d05f13b85a890e261cfa576c1c28e
maddie-wilkinson <144154893+maddie-wilkinson@users.noreply.github.com>
2023-12-04 22:00:40 -0600

    Update CalendarFragment.kt

--- app/src/main/java/com/example/taskmanager/CalendarActivity/CalendarFragment.kt
+++ app/src/main/java/com/example/taskmanager/CalendarActivity/CalendarFragment.kt
@@ -93,18 +93,10 @@ class CalendarFragment : Fragment(R.layout.calendar_fragment) {
                             binding.Calendar.notifyDateChanged(day.date)
                             oldDate?. let { binding.Calendar.notifyDateChanged(it) }
                             Log.d("Calendar Fragment", selectedDate.toString())
-
-                            // PROBABLY THE SPOT TO CALL METHOD FOR STARTING ADD TASK METHOD
-                            val fabAddTask = (activity as CalendarActivity).findViewById<FloatingActionButton>(R.id.fabAddTask)
-                            fabAddTask.setOnClickListener {
-                                Log.d("Task Button","add task button pressed")
-
-                                // Create intent to start AddTaskActivity
-                                val intent = Intent(activity, AddTaskActivity::class.java)
-                                intent.putExtra("selectedDate", selectedDate.toString())
-
-                                startActivity(intent)
-                            }
+                             val intent = Intent(activity, AddTaskActivity::class.java)
+                             intent.putExtra("selectedDate", selectedDate.toString())
+                             startActivity(intent)
+                    
                         }
                     }
                 }
